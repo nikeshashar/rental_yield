45fc4688ec177d16f80a8e27031ca7e9b5324feb
nikesh <nikeshashar@gmail.com>
2014-10-27 17:52:26 +0000

    Added test for creating property

--- spec/features/properties_features_spec.rb
+++ spec/features/properties_features_spec.rb
@@ -1,11 +1,24 @@
 require 'rails_helper'
 
-describe 'properties' do 
+describe 'Properties' do 
   context 'no properties added' do 
     it 'should display a prompt to add a new property' do 
       visit '/properties'
       expect(page).to have_content 'No properties yet!'
       expect(page).to have_link 'Add a property'
+    end
+  end
+
+  context 'properties have been added' do 
+    before do 
+      Property.create(postcode: 'EC1Y 1AA', rent: 1000, cost: 200000)
+    end
+
+    it 'should display them' do 
+      visit '/properties'
+      expect(page).to have_content 'EC1Y 1AA'
+      expect(page).to have_content '1000'
+      expect(page).to have_content '200,000'
     end
   end
 end
