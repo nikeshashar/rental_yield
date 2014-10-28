7b6efe000033d86f0c46636f5aba160dfd5aed7d
nikesh <nikeshashar@gmail.com>
2014-10-28 12:33:19 +0000

    You can now create a property with its postcode, initial cost and rental income

--- app/controllers/properties_controller.rb
+++ app/controllers/properties_controller.rb
@@ -2,4 +2,13 @@ class PropertiesController < ApplicationController
   def index
     @properties = Property.all
   end
+
+  def new 
+    @property = Property.new
+  end
+
+  def create
+    Property.create(params[:property].permit(:postcode, :cost, :rent))
+    redirect_to '/properties'
+  end
 end
