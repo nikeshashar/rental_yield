2ee9a9fc15d315dc312183a20cfd4dbf5cb6f1f2
nikesh <nikeshashar@gmail.com>
2014-10-29 10:26:10 +0000

    Added tests for devise and generated user model

--- spec/features/creating_properties_features_spec.rb
+++ spec/features/creating_properties_features_spec.rb
@@ -1,20 +1,36 @@
 require 'rails_helper'
 
-describe 'creating properties' do 
-  context 'a valid entry' do 
-    it 'shows a form and then displays the information' do 
-      visit '/properties'
-      click_link 'Add a property'
-      fill_in "Postcode", with: 'EC1Y 1AA'
-      fill_in 'Rent', with: '1000'
-      fill_in 'Cost', with: 200000
-      click_button 'Create Property'
-      expect(page).to have_content 'EC1Y 1AA'
-      expect(page).to have_content '1000'
-      expect(page).to have_content '200000'
-      expect(current_path).to eq '/properties'
-    end
+context 'logged out' do 
+  it 'should prompt to sign in' do 
+    visit '/properties'
+    click_link 'Add a property'
+    expect(page).to have_content 'Sign in'
   end
+end
+
+context 'logged in' do
+
+  before do 
+    john = User.create(email: "john@rentify.com", password: '12345678', password_confirmation: '12345678')
+    login_as john
+  end 
+  
+  describe 'creating properties' do 
+    context 'a valid entry' do 
+      it 'shows a form and then displays the information' do 
+        visit '/properties'
+        click_link 'Add a property'
+        fill_in "Postcode", with: 'EC1Y 1AA'
+        fill_in 'Rent', with: '1000'
+        fill_in 'Cost', with: 200000
+        click_button 'Create Property'
+        expect(page).to have_content 'EC1Y 1AA'
+        expect(page).to have_content '1000'
+        expect(page).to have_content '200000'
+        expect(current_path).to eq '/properties'
+      end
+    end
+
 
   context 'an invalid entry' do 
     it 'does not create a property if the postcode is too short' do 
@@ -26,4 +42,5 @@ describe 'creating properties' do
       expect(page).not_to have_content 'EC1Y'
     end
   end
+end
 end
